a56254cf271bdddd32778b2ae4526b058f0f11f3
Kelly Kitui <kituikelly@gmail.com>
2024-03-09 11:31:55 +0300

    Creates destructive and non destructive array methods(ARRAYs LAB)

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+   return cats.push(name);
+ }
+
+function destructivelyPrependCat(name){
+   return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+   return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+   return cats.shift(name)
+}
+let catsCpy;
+function appendCat(name){
+   return catsCpy= [...cats.slice(),name];
+}
+function prependCat(name){
+   return catsCpy=[name, ...cats.slice()]
+}
+function removeLastCat(){
+   return catsCpy=[...cats.slice(0, -1)]
+}
+function removeFirstCat(){
+   return catsCpy=[...cats.slice(1)]
+}
+
